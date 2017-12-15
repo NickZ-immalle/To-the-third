ce75967f6004643e8aa04130dcd9dc0bf03a7975
Nick Zegels <nick.zegels@immalle.eu>
2017-12-15 17:32:30 +0100

    1-10 to third

--- to the third/Program.cs
+++ to the third/Program.cs
@@ -10,6 +10,10 @@ namespace to_the_third
     {
         static void Main(string[] args)
         {
+            for(int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i * i * i);
+            }
         }
     }
 }
